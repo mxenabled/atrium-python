9a2797c480f1d79bf79c9721c7e9990368acbdbb
Brett Mortensen <brett.mortensen@mx.com>
2017-11-30 15:47:28 -0700

    changed output to match docs

--- atrium/models/challenge.py
+++ atrium/models/challenge.py
@@ -5,9 +5,15 @@ class Challenge:
     guid = None
     label = None
     type = None
+    image_data = None
+    options = None
 
     def __init__(self, response):
         self.field_name = response["field_name"]
         self.guid = response["guid"]
         self.label = response["label"]
         self.type = response["type"]
+        if "image_data" in response:
+            self.image_data = response["image_data"]
+        if "options" in response:
+            self.options = response["options"]
